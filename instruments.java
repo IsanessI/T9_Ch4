68961705ddeb28a09987c5a3a6f80b3e4cc5e3f0
lsanessl <saness729@gmail.com>
2023-11-10 16:53:23 +0300

    class of instruments

---
+++ instruments.java
@@ -0,0 +1,8 @@
+
+
+public interface instruments {
+	abstract public void set();
+	abstract public int hashCode();
+	abstract public String toString();
+	abstract public boolean equals(Object obj);
+}
